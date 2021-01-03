78398f089d1917771f6aec86ecacac79d202d991
Rajesh Panwar <rajesh.panwar@appwrk.com>
2021-01-03 18:14:30 +0530

    Added the new tables for logs and storing ENUM. Every Constant should be configurable and come from db.

---
+++ server/models/errorLog.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+	Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+	class ErrorLog extends Model {
+
+		static associate(models) {
+		}
+	};
+
+	ErrorLog.init({
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			primaryKey: true,
+		},
+		userId: {
+			type: DataTypes.INTEGER,
+			allowNull: true,
+		},
+		userEmail: {
+			type: DataTypes.STRING(255),
+			allowNull: true,
+		},
+		route: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		requestPayload: {
+			type: DataTypes.STRING(255),
+			allowNull: false,
+		},
+		requestJSON: { type: DataTypes.JSON, allowNull: false },
+		stackTrace: { type: DataTypes.JSON, allowNull: false },
+		
+	}, {
+		sequelize,
+		timestamps: true,
+		modelName: 'Role',
+	});
+	return ErrorLog;
+};

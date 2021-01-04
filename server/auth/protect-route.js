6f821f2a3b78633fe73bcab41799cec448133cc9
Rajesh Panwar <rajesh.panwar@appwrk.com>
2021-01-04 14:32:08 +0530

    temporary shortcircuit to disable IAM for APIs.

--- server/auth/protect-route.js
+++ server/auth/protect-route.js
@@ -1,15 +1,16 @@
 const ifOpen = require('./open-routes')
 module.exports = (action, route) => {
-  return (req, res, next) => {
-    if (ifOpen(req.originalUrl, req.method)) {
-      return next()
-    }
-    if (req.user && req.user.ability.can(action, route)) {
-      return next()
-    } else {
-      return res.status(403).json({
-        message: `You are not authorized to ${action} on ${route}`
-      })
-    }
-  }
+	return (req, res, next) => {
+		return next(); // temporary shortcircuit to disable IAM.
+		if (ifOpen(req.originalUrl, req.method)) {
+			return next()
+		}
+		if (req.user && req.user.ability.can(action, route)) {
+			return next()
+		} else {
+			return res.status(403).json({
+				message: `You are not authorized to ${action} on ${route}`
+			})
+		}
+	}
 }

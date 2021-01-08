7d2bd9f195aad89c6282a9525e744aee42c617a5
Rajesh Panwar <rajesh.panwar@appwrk.com>
2021-01-08 19:19:23 +0530

    Error handling fixes

--- server/controllers/errorLog.js
+++ server/controllers/errorLog.js
@@ -4,8 +4,8 @@ const ErrorLog = require('../models/').ErrorLog;
 module.exports.errorLogging = async (req, err) => {
 
   let payload = {
-    userId: req.user.id ? req.user.id : '',
-    userEmail: req.user.email ? req.user.email : '',
+    userId: req.user ? req.user.id : '',
+    userEmail: req.user? req.user.email : '',
     route: req.originalUrl ? req.originalUrl : '',
     requestPayload: JSON.stringify(req.body),
     // requestJSON: JSON.stringify(req),
